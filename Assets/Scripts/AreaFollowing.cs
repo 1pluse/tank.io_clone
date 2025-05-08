6ad8d3dbc8f1b894128665d2c0846e5e512a6178
Kang Seungmin <sg5830866@gmail.com>
2025-05-08 23:23:42 +0900

    무한맵 구현,Area객체의 Colider가 Player를 따라오는 로직 구현

---
+++ Assets/Scripts/AreaFollowing.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AreaFollowing : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigid.MovePosition((Vector2)player.position);
+    }
+}
